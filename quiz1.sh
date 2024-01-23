e3415ec9dc9102b92962d87b3805258ac365d332
ishasuba <ishasuba9@gmail.com>
2024-01-23 10:41:45 -0800

    quiz1

--- quiz1.sh
+++ quiz1.sh
@@ -1,4 +1,7 @@
 echo "real name: Isha Suba"
 echo "Linux username: ishasuba"
 cd ~/Code/MCB185/data
-gunzip -c dictionary.gz | grep "a" | grep -v "[bdeghijklnpqrsvwxyz]" | wc -l 
+gunzip -c dictionary.gz | grep "a" | grep -v "[bdeghijklnpqrsvwxyz]" | wc -l
+gunzip -c dictionary.gz | grep "b" | grep -v "[cdefghjkmopqsuvwxyz]" | wc -l
+gunzip -c dictionary.gz | grep "c" | grep -v "[befghjklpqrstuvwxyz]" | wc -l
+gunzip -c dictionary.gz | grep "z" | grep -v "[bcdefhjklmpqstuvwxy]" | wc -l
