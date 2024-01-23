b13243f2fd5ed1d9988626a40537b03c965b9dcf
ishasuba <ishasuba9@gmail.com>
2024-01-23 10:09:54 -0800

    quiz1

--- quiz1.sh
+++ quiz1.sh
@@ -1,2 +1,4 @@
-real name: Isha Suba
-Linux username: ishasuba
+echo "real name: Isha Suba"
+echo "Linux username: ishasuba"
+cd ~/Code/MCB185/data
+gunzip -c dictionary.gz | grep "a" | grep -v "[bdeghijklnpqrsvwxyz]" | wc -l 
