e14257df1162c293da1e180d23753183fb3302f6
ishasuba <ishasuba9@gmail.com>
2024-02-06 09:31:36 -0800

    quiz

--- 38quiz.py
+++ 38quiz.py
@@ -1,1 +1,14 @@
 # 38quiz.py by Isha Suba, Co-authors: Francesca Gasperini, Amina Muhic
+
+def GregoryLeibniz(n): 
+	sign = (-1)
+	d = 3
+	x = 0
+	for i in range(n): 
+		x = (sign*1) / d
+		sign = sign * (-1)
+		d = d + 2
+	return 1 - x
+
+print(GregoryLeibniz(6))
+		
