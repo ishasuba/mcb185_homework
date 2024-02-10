2b7219a4dbe995253c47cddf8d1373db7b324dbe
ishasuba <ishasuba9@gmail.com>
2024-02-10 14:33:52 -0800

    unit4update

--- 43randomna.py
+++ 43randomna.py
@@ -3,7 +3,7 @@ import random
 sequence_num = 0
 for sequence_num in range(5):
 	print("seq-", sequence_num + 1)
-	for i in range(60):
+	for i in range(random.randint(40, 60)):
 		bases = 'ATCG'
 		print(random.choice(bases), end='')
 	print("\n")
