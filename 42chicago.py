1a25ce5ce05f3fbe87795d540b7b1bbb1a4e9888
ishasuba <ishasuba9@gmail.com>
2024-02-10 14:26:30 -0800

    unit4

---
+++ 42chicago.py
@@ -0,0 +1,17 @@
+import random 
+import sys
+
+games = 1000000
+log = games // 100
+total = 0
+zeroes = 0 
+for i in range(games):
+	if i % log == 0: print(f'{100 * i/games:.0f}', file=sys.stderr)
+	score = 0
+	for target in range(2, 13):
+		if random.randint(1, 6) + random.randint(1, 6) == target: 
+			score += target
+	if score == 0: zeroes += 1
+	total += score
+print(total / games)
+print(zeroes / games)
