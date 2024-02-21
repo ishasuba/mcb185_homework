ddc09e681bb76a385fa5049b4141f008656b97db
ishasuba <ishasuba9@gmail.com>
2024-02-20 22:52:35 -0800

    unit5

---
+++ 55colorname.py
@@ -0,0 +1,25 @@
+import sys
+
+colorfile = sys.argv[1]
+R = int(sys.argv[2])
+G = int(sys.argv[3])
+B = int(sys.argv[4])
+
+min_diff = 1000
+with open(colorfile) as fp:
+	for line in fp:
+		words = line.split('\t')
+		colors = words[0]
+		num = words[2].split(',')
+		red = int(num[0])
+		green = int(num[1])
+		blue = int(num[2])
+		diff = abs(R - red) + abs(G - green) + abs(B - blue)
+		if diff < min_diff:
+			min_diff = diff
+			closest_color = colors
+print(closest_color)
+			
+		
+		
+		
